ad592c226219ea49860bb159e86a08083de270b5
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-03-31 15:54:33 -0400

    added functionality for modal to pop up on mouse leave and ability to close modal with close box, but also limit pop up to one time per page load

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 
 window.onload = function(){
+  let emailState = false;
+
   let emailModal = document.getElementsByClassName('email-modal')[0]
 
   let closeModal = document.getElementsByClassName('email-modal__close-btn')[0]
   
+  let showModal = () => {
+    if(emailState == false){
+      emailModal.classList.add('email-modal--visible');
+      emailState = true
+    }
+  }
   closeModal.addEventListener('click', () => {
     emailModal.classList.remove('email-modal--visible');
-  })
+  });
+
+ 
+    document.body.addEventListener('mouseleave', () => {
+      showModal();
+    });
   
-  document.addEventListener('mouseleave', () => {
-    emailModal.classList.add('email-modal--visible')
-    console.log('mouse left')
-  })
+  
   console.log(document)
 }
